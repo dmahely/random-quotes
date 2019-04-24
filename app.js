851389f373ca74538e82896e188ae7e51879a7a2
Doaa Mahely <dom.k9@hotmail.com>
2019-04-25 01:05:09 +0400

    Links html to css file and cleans up js

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-// https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js
+// changes the background color to a random color
 function random_bg_color() {
-    var x = Math.floor(Math.random() * 256);
-    var y = Math.floor(Math.random() * 256);
-    var z = Math.floor(Math.random() * 256);
-    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
- // console.log(bgColor);
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+    let bgColor = "rgb(" + x + "," + y + "," + z + ")";
 
   document.body.style.background = bgColor;
   document.getElementById("quote").style.color = bgColor;
@@ -12,13 +11,14 @@ function random_bg_color() {
   document.getElementById("byline").style.color = bgColor;
   document.getElementById("twitter").style.color = bgColor;
   document.getElementById("newQuote").style.background = bgColor;
-    }
+}
 
+// generates a random quote
 function randomQuotes() {
   $.ajax({
     url: 'http://quotesondesign.com/wp-json/posts',
     success: function(result) {
-      $("#actualQuote").html(JSON.stringify(result[0].content));
+      $("#actualQuote").html(JSON.stringify(result[0].content).replace(/\<.\>|\n|\<\/.\>/gi, ''));
       console.log("HTML of ACTUAL QUOTES IS " + $("#actualQuote").html());
       $("#byline").html("by " + JSON.stringify(result[0].title).replace(/"/g,""));
       // console.log("HTML of ACTUAL QUOTES IS " + String($("#actualQuote").html()));
