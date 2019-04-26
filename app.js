602952cd378dcf50dbe9dc8fed9d96e42fc71924
Doaa Mahely <dom.k9@hotmail.com>
2019-04-27 01:31:35 +0400

    Fixes encoding issue

--- app.js
+++ app.js
@@ -18,16 +18,18 @@ function randomQuotes() {
   $.ajax({
     url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
     success: function(result) {
-      $("#actualQuote").html('"' + JSON.stringify(result[0].content).replace(/\<.\>|\\n.|\s\s+|"|\<\/.\>/gi, '') + '"');
-      console.log("HTML of ACTUAL QUOTES IS " + $("#actualQuote").html());
+      let quote = '"' + JSON.stringify(result[0].content).replace(/\<.\>|\\n.|\s\s+|"|\<\/.\>/gi, '') + '"';
+      console.log("Quote: " + quote);
+      // console.log("HTML of ACTUAL QUOTES IS " + $("#actualQuote").html());
       $("#byline").html("by " + JSON.stringify(result[0].title).replace(/"/g, ""));
       random_bg_color();
+      $("#actualQuote").html(quote);
+      let author = $("#byline").html();
 
-      $("#twitter").click(function tweetItOut(){
-        let quote = $("#actualQuote").html();
-        let author = $("#byline").html();
-        var url = "https://twitter.com/intent/tweet";
+      $("#twitter").click(function tweetItOut() {
+        let url = "https://twitter.com/intent/tweet";
+        console.log(author + ": " + quote);
         window.open(url + "?text=" + quote + "%20" + author);
       });
-}, cache: false});
+  }, cache: false});
 }
